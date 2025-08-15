e0927fca56ad5d9452c0331ab9ff5a352cd18ec6
michiel1106 <michielimmink@gmail.com>
2025-08-15 20:55:02 +0200

    initial commit. textures dont work

--- src/main/java/com/example/SampleMod112.java
+++ src/main/java/com/example/SampleMod112.java
@@ -1,7 +1,18 @@
 package com.example;
 
+import com.example.item.CustomArmor;
+
+import com.example.models.ModelBloodedLightningDragonArmor;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,14 +22,53 @@ import org.apache.logging.log4j.Logger;
 	version = SampleMod112.VERSION
 )
 public class SampleMod112 {
-	public static final String MODID = "samplemod112";
-	public static final String NAME = "Sample Mod 1.12";
+	public static final String MODID = "bcarmors";
+	public static final String NAME = "Better Custom Armor";
 	public static final String VERSION = "1.0";
+
+
+	public static final ModelBloodedLightningDragonArmor ARMOR_MODEL_NO_LEGS = new ModelBloodedLightningDragonArmor(0.5F, false);
+	public static final ModelBloodedLightningDragonArmor ARMOR_MODEL_LEGS = new ModelBloodedLightningDragonArmor(0.2F, true);
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+
+
+	public static ItemArmor.ArmorMaterial ARMOR_MATERIAL = EnumHelper.addArmorMaterial("BETTER_ARMOR",
+			"bcarmors:textures/models/armor/better_armor",
+			800,
+			new int[] {7, 10, 13, 7},
+			20,
+			SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND,
+			2.0F);
+
+
+
+
+
+	public static final Item BETTER_HELMET = new CustomArmor(ARMOR_MATERIAL, 1, EntityEquipmentSlot.HEAD);
+	public static final Item BETTER_CHESTPLATE = new CustomArmor(ARMOR_MATERIAL, 1, EntityEquipmentSlot.CHEST);
+	public static final Item BETTER_LEGGINGS = new CustomArmor(ARMOR_MATERIAL, 2, EntityEquipmentSlot.LEGS);
+	public static final Item BETTER_BOOTS = new CustomArmor(ARMOR_MATERIAL, 1, EntityEquipmentSlot.FEET);
+
 	
 	@Mod.EventHandler
 	public void preinit(FMLPreInitializationEvent preinit) {
-		LOGGER.info("Hello, world!");
+		MinecraftForge.EVENT_BUS.register(this);
+
+
+    }
+
+
+	@SubscribeEvent
+	public void registerItems(RegistryEvent.Register<Item> event) {
+
+		event.getRegistry().register(BETTER_HELMET.setRegistryName(MODID, "ender_dragon_head_piece").setTranslationKey("ender_dragon_head_piece"));
+		event.getRegistry().register(BETTER_CHESTPLATE.setRegistryName(MODID, "rahovart_chest_piece").setTranslationKey("rahovart_chest_piece"));
+		event.getRegistry().register(BETTER_LEGGINGS.setRegistryName(MODID, "amalgalich_leggings").setTranslationKey("amalgalich_leggings"));
+		event.getRegistry().register(BETTER_BOOTS.setRegistryName(MODID, "asmodeus_boots").setTranslationKey("asmodeus_boots"));
+
 	}
+
+
 }
---
+++ src/main/java/com/example/item/CustomArmor.java
@@ -0,0 +1,53 @@
+package com.example.item;
+
+import com.example.SampleMod112;
+import com.example.models.ModelBloodedLightningDragonArmor;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class CustomArmor extends ItemArmor {
+    public CustomArmor(ArmorMaterial material, int render, EntityEquipmentSlot equipmentSlot) {
+        super(material, render, equipmentSlot);
+
+    }
+
+
+    @Override
+    public boolean isEnchantable(ItemStack itemStack) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot, ModelBiped _default) {
+
+
+        if (renderIndex == 2) {
+            return SampleMod112.ARMOR_MODEL_LEGS;
+        } else {
+            return SampleMod112.ARMOR_MODEL_NO_LEGS;
+        }
+
+    }
+
+
+    public String getArmorTexture(ItemStack stack, Entity entity, EntityEquipmentSlot slot, String type) {
+        return "bcarmors:textures/models/armor/" + "armor_black" + (renderIndex == 2 ? "_legs.png" : ".png");
+    }
+
+
+}
